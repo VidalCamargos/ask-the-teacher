09e18b3ea5eab28719b917fa08d33a70f3731f3c
vidalCamargos <root@Gustavo>
2024-11-21 22:07:33 -0300

    creating seeders and factory for question model

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,6 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function questions(): HasMany
     {
-        return $this->hasMany(Question::class, 'created_by_id');
+        return $this->hasMany(Question::class);
     }
 }
