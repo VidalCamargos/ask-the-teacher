d94248a8a82e9f4925f9a16f866c95663a82440c
vidalCamargos <root@Gustavo>
2024-11-25 21:10:45 -0300

    add draft column and cast

--- database/migrations/2024_11_09_165934_create_questions_table.php
+++ database/migrations/2024_11_09_165934_create_questions_table.php
@@ -11,6 +11,7 @@ return new class () extends Migration {
             $table->id();
             $table->foreignId('created_by_id');
             $table->text('question');
+            $table->boolean('draft')->default(false);
 
             $table->timestamps();
         });
