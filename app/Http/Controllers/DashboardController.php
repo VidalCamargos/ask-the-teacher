a99ad872403bb4050ca7e0530d115ace298a572f
vidalCamargos <root@Gustavo>
2024-11-29 22:07:12 -0300

    apply filter in the front end

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
 
 class DashboardController extends Controller
 {
     public function __invoke(): View
     {
         return view('dashboard', [
-            'questions' => Question::where('draft', false)->withSum('votes', 'like')
+            'questions' => Question::where('draft', false)
+                ->when(
+                    request()->has('search'),
+                    function (Builder $query) {
+                        $query->where('question', 'like', '%' . request()->search . '%');
+                    }
+                )
+                ->withSum('votes', 'like')
                 ->withSum('votes', 'unlike')
                 ->orderByRaw('
                     case when votes_sum_like is null then 0 else votes_sum_like end desc,
